b6010e2d9f7d0cfc2d2f8f2aa7d82a299cdcb5c2
Sebastián <sebadlf@gmail.com>
2020-11-15 20:42:59 -0300

    Adding dato_actual for okex

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-tickers = ['BTC', 'ETH', 'LTC', 'ETC', 'XRP', 'EOS', 'BCH', 'BSV', 'TRX']
+from datetime import datetime, timedelta
+
+TICKERS = ['BTC', 'ETH', 'LTC', 'ETC', 'XRP', 'EOS', 'BCH', 'BSV', 'TRX']
+
+FIRST_ROW_DATE = datetime.utcnow() - timedelta(days=1)
--- okex.py
+++ okex.py
@@ -1,77 +1,34 @@
-import requests
-import pandas as pd
 from db import BD_CONNECTION
 from sqlalchemy import create_engine
-engine = create_engine(BD_CONNECTION)
-from datetime import datetime, timedelta
+from okex_utils import crear_tabla, dato_historico
+from config import TICKERS, FIRST_ROW_DATE
+from utils import get_last_row, delete_last_row
+from datetime import timedelta
 
-#tickers = ['BTC']
+db_connection = create_engine(BD_CONNECTION)
 
-create_table = '''
-CREATE TABLE IF NOT EXISTS `okex` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ticker` varchar(20) DEFAULT '',
-  `time` timestamp NULL DEFAULT NULL,
-  `open` double DEFAULT NULL,
-  `high` double DEFAULT NULL,
-  `low` double DEFAULT NULL,
-  `close` double DEFAULT NULL,
-  `volume` double DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_ticker_time` (`ticker`,`time`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-'''
-engine.execute(create_table)
+crear_tabla(db_connection)
 
-for ticker in tickers:
+for ticker in TICKERS:
 
     print(ticker)
 
     finished = False
+
     while not finished:
 
-        url = f'https://okex.com/api/spot/v3/instruments/{ticker}-USDT/history/candles'
+        last_row = get_last_row(conn=db_connection, table_name='okex', ticker=ticker)
 
-        busquedaUltimaFecha = f'SELECT `id`,`time` FROM okex WHERE `ticker` = "{ticker}" ORDER BY `time` DESC limit 0,1'
-        ultimaFecha = engine.execute(busquedaUltimaFecha).fetchone()
+        if last_row.get('id'):
+            delete_last_row(conn=db_connection, table_name='okex', id=last_row['id'])
 
-        end = datetime.now() - timedelta(days=100)
+        since = last_row.get('time') if last_row.get('time') else FIRST_ROW_DATE
 
-        if (ultimaFecha):
-            id = ultimaFecha[0]
-            end = ultimaFecha[1]
+        to = since + timedelta(minutes=3000)
 
-            query_borrado = f'DELETE FROM okex WHERE `id`={id}'
-            engine.execute(query_borrado)
+        df = dato_historico(ticker, 'USDT', desde=since, hasta=to, timeframe="1m")
 
-        start = end + timedelta(minutes=300 - 1)
-
-        params = {
-            'start': start.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
-            'end': end.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
-            'granularity': 60
-        }
-
-        print(params)
-
-        r = requests.get(url, params=params)
-        js = r.json()
-        df = pd.DataFrame(js)
-
-        df.columns = ['time', 'open', 'high', 'low', 'close', 'volume']
-
-        df.time = pd.to_datetime(df.time)
-        df.open = df.open.astype(float)
-        df.high = df.high.astype(float)
-        df.low = df.low.astype(float)
-        df.close = df.close.astype(float)
-        df.volume = df.volume.astype(float)
-
-        df['ticker'] = ticker
-
-        df.set_index('time', inplace=True)
-
-        df.to_sql('okex', engine, if_exists='append')
+        df.to_sql('okex', db_connection, if_exists='append')
 
         finished = len(df) < 2
 
