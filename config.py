b2e1aeac832bf09a674138fadaa04af011219127
Sebastián <sebadlf@gmail.com>
2020-11-15 20:44:39 -0300

    Adding dato_actual for okex

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from datetime import datetime, timedelta
 
 TICKERS = ['BTC', 'ETH', 'LTC', 'ETC', 'XRP', 'EOS', 'BCH', 'BSV', 'TRX']
 
-FIRST_ROW_DATE = datetime.utcnow() - timedelta(days=1)
+FIRST_ROW_DATE = datetime.utcnow() - timedelta(weeks=26)
